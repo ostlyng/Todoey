b6b8cb012dfc36c5c4202b4d723a05ef53ffa930
Thomas Østlyng <t.ostlyng@icloud.com>
2018-06-21 18:29:28 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Anne Kristine on 21.06.2018.
+//  Copyright © 2018 Thomas Østlyng. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+	var title: String = ""
+	var done: Bool = false
+}
